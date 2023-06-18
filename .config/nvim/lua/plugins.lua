5291d78f93611c1f52ee7095e34e7f3fc2bf9aa4
Raphael Seher <raphael.seher@gmail.com>
2023-06-18 23:26:06 +0200

    Add debugger for nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -47,4 +47,11 @@ return require("packer").startup(function(use)
 
 	use("mfussenegger/nvim-dap")
 	use("rcarriga/nvim-dap-ui")
+	use("mxsdev/nvim-dap-vscode-js")
+	use({
+		"microsoft/vscode-js-debug",
+		opt = true,
+		run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+	})
+	use("leoluz/nvim-dap-go")
 end)
