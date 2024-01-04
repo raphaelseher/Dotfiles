dc4210239a6a617d1a97474665c724f3a71c5b1e
Raphael Seher <RSeher@anexia-it.com>
2024-01-04 08:50:24 +0100

    Reformat & disable psalm linter

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -55,37 +55,42 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("lsp_attach_auto_diag", { clear = true }),
-	callback = function(args)
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local bufopts = { noremap = true, silent = true, buffer = args.buf }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-		-- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-		-- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-		-- vim.keymap.set('n', '<leader>wl', function()
-		-- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		-- end, bufopts)
-		-- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-		vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-		vim.keymap.set("n", "<leader>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, bufopts)
+    group = vim.api.nvim_create_augroup("lsp_attach_auto_diag", { clear = true }),
+    callback = function(args)
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { noremap = true, silent = true, buffer = args.buf }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+        -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        -- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        -- vim.keymap.set('n', '<leader>wl', function()
+        -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        -- end, bufopts)
+        -- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ async = true })
+        end, bufopts)
 
-		vim.api.nvim_set_keymap("n", "<leader>h", "lua ClangdSwitchSourceHeaderForCpp()<CR>", {})
-	end,
+        vim.api.nvim_set_keymap("n", "<leader>h", "lua ClangdSwitchSourceHeaderForCpp()<CR>", {})
+    end,
 })
 
 function ClangdSwitchSourceHeaderForCpp()
-	vim.api.nvim_command(":ClangdSwitchSourceHeader")
+    vim.api.nvim_command(":ClangdSwitchSourceHeader")
 end
 
 vim.api.nvim_command(
-	"autocmd FileType c,cpp,h,hpp nnoremap <buffer> <leader>h :lua ClangdSwitchSourceHeaderForCpp()<CR>"
+    "autocmd FileType c,cpp,h,hpp nnoremap <buffer> <leader>h :lua ClangdSwitchSourceHeaderForCpp()<CR>"
 )
+
+vim.api.nvim_set_keymap("n", "<leader>al",
+    [[:cexpr system("./docker/bin/tools.sh test psalm-diff origin/staging | grep -v 'No problems found*' |  grep -vE '^[0-9]+/[0-9]+: Checking file' | grep -v 'Checking *'")
+<CR>]], {})
+vim.api.nvim_set_keymap("n", "<leader>as", ":Telescope resume<CR>", {})
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,17 +4,17 @@ return {
     cmd = { "ConformInfo" },
     keys = {
         {
-          -- Customize or remove this keymap to your liking
-          "<leader>f",
-          function()
-            require("conform").format({ async = true, lsp_fallback = true })
-          end,
-          mode = "",
-          desc = "Format buffer",
+            -- Customize or remove this keymap to your liking
+            "<leader>f",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
         },
-      },
+    },
     opts = {
-          formatters_by_ft = {
+        formatters_by_ft = {
             lua = { "stylua" },
             php = { "blade-formatter", "php_cs_fixer" },
             cpp = { "clang-format" },
@@ -24,7 +24,7 @@ return {
             html = { { "prettierd", "prettier" } },
             ["*"] = { "codespell" },
             ["_"] = { "trim_whitespace" },
-          },
+        },
         format_on_save = { timeout_ms = 500, lsp_fallback = true },
     }
 }
