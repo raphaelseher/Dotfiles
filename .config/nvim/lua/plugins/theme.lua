1e4ac77560450524182866f295b4957beab61c82
Raphael Seher <RSeher@anexia-it.com>
2024-01-22 15:07:22 +0100

    Change back to robbyrussel & dracula

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,13 +1,20 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false, 
-		priority = 1000, 
-		config = function()
-			vim.cmd("syntax enable")
-			vim.cmd("colorscheme tokyonight")
-		end,
-	},
-
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("syntax enable")
+            --vim.cmd("colorscheme tokyonight")
+        end,
+    },
+    {
+        "https://github.com/Mofiqul/dracula.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("syntax enable")
+            vim.cmd("colorscheme dracula")
+        end,
+    }
 }
-
