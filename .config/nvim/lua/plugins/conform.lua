5779c8dd67d9c7b31793020c1fd985e20ec3d227
Raphael Seher <RSeher@anexia-it.com>
2024-02-28 09:01:18 +0100

    Add autoformat setting commands

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,30 +1,36 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            -- Customize or remove this keymap to your liking
-            "<leader>f",
-            function()
-                require("conform").format({ async = true, lsp_fallback = true })
-            end,
-            mode = "",
-            desc = "Format buffer",
-        },
-    },
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            php = { "php_cs_fixer", "blade-formatter" },
-            cpp = { "clang-format" },
-            go = { "goimports", "gofmt" },
-            javascript = { { "prettierd", "prettier" } },
-            typescript = { { "prettierd", "prettier" } },
-            html = { { "prettierd", "prettier" } },
-            ["*"] = { "codespell" },
-            ["_"] = { "trim_whitespace" },
-        },
-        format_on_save = { timeout_ms = 500, lsp_fallback = true },
-    }
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			php = { "php_cs_fixer", "blade-formatter" },
+			cpp = { "clang-format" },
+			go = { "goimports", "gofmt" },
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			html = { { "prettierd", "prettier" } },
+			["*"] = { "codespell" },
+			["_"] = { "trim_whitespace" },
+		},
+		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { timeout_ms = 500, lsp_fallback = true }
+		end,
+	},
 }
