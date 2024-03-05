3047d5b9aa7d6e2fde7c05c85a09acdfe3d570c5
Raphael Seher <raphael.seher@gmail.com>
2024-03-05 21:20:02 +0100

    Add special case for blade files

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,30 +1,39 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            -- Customize or remove this keymap to your liking
-            "<leader>f",
-            function()
-                require("conform").format({ async = true, lsp_fallback = true })
-            end,
-            mode = "",
-            desc = "Format buffer",
-        },
-    },
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            php = { "php_cs_fixer", "blade-formatter" },
-            cpp = { "clang-format" },
-            go = { "goimports", "gofmt" },
-            javascript = { { "prettierd", "prettier" } },
-            typescript = { { "prettierd", "prettier" } },
-            html = { { "prettierd", "prettier" } },
-            ["*"] = { "codespell" },
-            ["_"] = { "trim_whitespace" },
-        },
-        format_on_save = { timeout_ms = 500, lsp_fallback = true },
-    }
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			php = function(bufnr)
+				local filename = vim.api.nvim_buf_get_name(bufnr)
+				local end_str = string.sub(filename, -9)
+				if end_str == "blade.php" then
+					return { "blade-formatter" }
+				end
+
+				return { "php_cs_fixer" }
+			end,
+			cpp = { "clang-format" },
+			go = { "goimports", "gofmt" },
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			html = { { "prettierd", "prettier" } },
+			["*"] = { "codespell" },
+			["_"] = { "trim_whitespace" },
+		},
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		log_level = vim.log.levels.DEBUG,
+	},
 }
--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,12 +1,11 @@
 return {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
 		lint.linters_by_ft = {
 			lua = { "luacheck" },
-			markdown = { "vale" },
 			cpp = { "cppcheck", "clazy" },
 			php = { "phpcs" },
 			html = { "eslint" },
@@ -14,11 +13,11 @@ return {
 			typescript = { "eslint" },
 		}
 
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-            callback = function()
-                lint.try_lint()
-            end,
-        })
-    end,
-    opts = {},
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+	opts = {},
 }
