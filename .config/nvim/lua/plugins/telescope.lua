6ec4a3599bde295770eeb28c5972d8033864fa23
Raphael Seher <raphael.seher@gmail.com>
2024-09-22 20:32:59 +0200

    Change telescope to flex layout

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,37 @@
 return {
-	{ 
-        "nvim-telescope/telescope.nvim",
-        tag = '0.1.5',
-        lazy=false,
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function() 
-            require("telescope").setup({
-                defaults = {
-                    file_ignore_patterns = {
-                        "node_modules",
-                        "vendor",
-                    },
-                },
-            })
-        end
-    }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		lazy = false,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"node_modules",
+						"vendor",
+					},
+					preview = {
+						filesize_limit = 0.5, -- MB
+					},
+					layout_strategy = "flex",
+					layout_config = {
+						flex = {
+							flip_columns = 220,
+						},
+						vertical = {
+							prompt_position = "top", -- Keeps the search prompt at the top
+							preview_height = 0.7, -- Adjust the height of the preview window (40% of total height)
+							mirror = true, -- Puts the preview below the results
+						},
+					},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+			})
+		end,
+	},
 }
