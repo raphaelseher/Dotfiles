0b8c977f125a2cada583fbf83b25849fcec4ba72
Raphael Seher <RSeher@anexia-it.com>
2024-10-03 20:35:54 +0200

    Add secondary polybar bar settings

--- .config/polybar/forest/launch.sh
+++ .config/polybar/forest/launch.sh
@@ -10,8 +10,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
+# Get the primary monitor
+PRIMARY_MONITOR=$(xrandr --query | grep " primary" | cut -d" " -f1)
+
+# Launch Polybar on all monitors
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar -q main -c "$DIR"/config.ini --reload &
+  if [[ "$m" == "$PRIMARY_MONITOR" ]]; then
+    MONITOR=$m polybar -q main -c "$DIR"/config.ini --reload &
+  else
+    MONITOR=$m polybar -q secondary -c "$DIR"/config.ini --reload &
+  fi
 done
 
