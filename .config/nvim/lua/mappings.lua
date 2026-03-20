5d20f5b01d3a221140a074f9a9e7106ff255e7e1
Raphael Seher <rseher@anexia-it.com>
2026-03-20 14:31:26 +0100

    Move references mapping to a better place

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -69,6 +69,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
 		-- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
 		-- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
@@ -79,7 +80,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
 		vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 
 		vim.api.nvim_set_keymap("n", "<leader>h", "lua ClangdSwitchSourceHeaderForCpp()<CR>", {})
 	end,
