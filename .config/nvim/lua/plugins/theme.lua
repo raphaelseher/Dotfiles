6a2145a8c2866b181622d4d64b517fe6339f001b
Raphael Seher <raphael.seher@gmail.com>
2026-04-21 08:50:24 +0200

    Change to nightowl

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		config = function()
-			vim.cmd("colorscheme tokyonight")
+			-- vim.cmd("colorscheme tokyonight")
 		end,
 	},
 	{
@@ -26,6 +26,16 @@ return {
 				style = "deep",
 			})
 			--require("onedark").load()
+		end,
+	},
+	{
+		"oxfist/night-owl.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			require("night-owl").setup()
+			vim.cmd.colorscheme("night-owl")
 		end,
 	},
 	-- {
